82c1535614439eb6d011d5fe09e4c9abd3fb7201
imouto-token <159984401+imouto-token@users.noreply.github.com>
2024-03-17 12:12:41 -0400

    Add files via upload
    Merge Sorted Array

---
+++ no88.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int x = m - 1; 
+        int y = n - 1;
+        int z = (m + n) - 1;
+
+        while (x >= 0 && y >= 0) {
+            if (nums1[x] > nums2[y]) {
+                nums1[z] = nums1[x];
+                x--;
+            } else {
+                nums1[z] = nums2[y];
+                y--;
+            }
+            z--;
+        }
+
+        while (y >= 0) {
+            nums1[z] = nums2[y];
+            y--;
+            z--;
+        }
+    }
+}
